6d97829779940e10d0a284fdd4d40249d595320f
Amr <amr.essam67@gmail.com>
2018-02-16 15:46:50 +0200

    destroy and create features tests

---
+++ spec/factories/comment.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :comment do
+    sequence(:comment) {|n| "this is comment ##{n}"}
+    # commentable_id {Post.first.id || create(:post).id}
+    # commentable_type "Post"
+    user {User.first || association(:user)}
+  end
+end
