cdd7a5643bca9068b826285e119200464732c6de
Amr <amr.essam67@gmail.com>
2018-02-17 20:03:28 +0200

    more tests for reply

--- spec/features/replies_spec.rb
+++ spec/features/replies_spec.rb
@@ -26,5 +26,18 @@ RSpec.feature "Reply", type: :feature, js: true do
         expect(page).to have_content("this is my first reply section")
       end
     end
+    it "replies visible only within a specific comment" do
+      user = create(:user)
+      create(:post)
+      create_list(:comment, 3)
+      sign_in!(user)
+      visit "posts/#{Post.first.id}"
+      within "#comment-#{Comment.second.id}" do
+        fill_in "reply[comment]",with: "this is my first reply section"
+        click_on "reply"
+        expect(page).to have_content("this is my first reply section")
+      end
+      expect(page.all("p", text:"this is my first reply section").count).to eq(1)
+    end
   end
 end
